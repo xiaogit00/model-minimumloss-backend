2051d4fad5ddb7177d6e23c72b63ae202599fcb9
xiaogit00 <leibingguo@gmail.com>
2026-02-25 18:58:13 +0800

    some changes

--- models/5_SPY_CNN_24FEB26/MODEL_DEFINITION.py
+++ models/5_SPY_CNN_24FEB26/MODEL_DEFINITION.py
@@ -30,3 +30,27 @@ class StockCNN(nn.Module):
         out = self.flatten(out)
         # print("flatten out size:", out.size())
         return self.fc(out)
+    
+####
+StockCNN(
+  (layer1): Sequential(
+    (0): Conv1d(5, 16, kernel_size=(5,), stride=(1,), padding=(1,))
+    (1): BatchNorm1d(16, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+    (2): SiLU()
+    (3): MaxPool1d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+  )
+  (layer2): Sequential(
+    (0): Conv1d(16, 8, kernel_size=(5,), stride=(1,), padding=(1,))
+    (1): BatchNorm1d(8, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+    (2): SiLU()
+    (3): MaxPool1d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+  )
+  (flatten): Flatten(start_dim=1, end_dim=-1)
+  (fc): Sequential(
+    (0): Linear(in_features=104, out_features=20, bias=True)
+    (1): SiLU()
+    (2): Dropout(p=0.5, inplace=False)
+    (3): Linear(in_features=20, out_features=1, bias=True)
+    (4): Sigmoid()
+  )
+)
